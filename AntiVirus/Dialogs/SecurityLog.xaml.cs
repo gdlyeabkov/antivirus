b7bb198db68ad302dc380de71efa01e9c9aafabe
gdlyeabkov <gdlyeabkov@gmail.com>
2021-12-25 18:33:56 +0300

    add_virus_scanner

--- AntiVirus/Dialogs/SecurityLog.xaml.cs
+++ AntiVirus/Dialogs/SecurityLog.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,11 @@ namespace AntiVirus.Dialogs
         public SecurityLog()
         {
             InitializeComponent();
+
+            // CreateSecurityLogHanler();
+
+            GetLogsHandler();
+
         }
 
         private void CloseProgrammHandler(object sender, MouseButtonEventArgs e)
@@ -37,6 +43,98 @@ namespace AntiVirus.Dialogs
         private void CancelHandler(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void CreateSecurityLogHanler()
+        {
+            DateTime now = DateTime.Now;
+            string parsedDate = now.Day.ToString() + "." + now.Month.ToString() + "." + now.Year.ToString() + "T" + now.Hour.ToString() + ":" + now.Minute.ToString() + ":" + now.Second;
+            string logDataHeader = "Проверка файлов";
+            int threatsCount = 0;
+            string logDataDesc = "Обнаружено " + threatsCount.ToString() + " угроз";
+            string logDataSeparator = "%";
+            string logData = logDataHeader + logDataSeparator + logDataDesc;
+            string log = logData + "@" + parsedDate;
+            debug.Text = log;
+            Stream myStream;
+            bool isLogExists = File.ReadAllLines(@"C:\antivirus_logs\logs.txt").Length >= 1;
+            if ((myStream = File.Open(@"C:\antivirus_logs\logs.txt", FileMode.Append)) != null)
+            {
+                using (StreamWriter sw = new StreamWriter(myStream))
+                {
+                    string separator = "";
+                    if (isLogExists)
+                    {
+                        separator = "\n";
+                    }
+                    sw.Write(separator + log);
+                }
+            }
+        }
+
+        // private void GetLogsHandler(object sender, MouseEventArgs e)
+        private void GetLogsHandler()
+        {
+            Stream myStream;
+            if ((myStream = File.Open(@"C:\antivirus_logs\logs.txt", FileMode.Open)) != null)
+            {
+
+                myStream.Close();
+                string file_text = File.ReadAllText(@"C:\antivirus_logs\logs.txt");
+                string[] rawLogs = file_text.Split(new Char[] { '\n' });
+                securityLogs.RowDefinitions.RemoveRange(1, 7);
+                bool isDisplayLogs = rawLogs.ToList().TrueForAll((rawLog) => rawLog.Length >= 5);
+                if (isDisplayLogs) {
+                    foreach (string rawLog in rawLogs)
+                    {
+                        RowDefinition row = new RowDefinition();
+                        row.Height = new GridLength(35);
+                        securityLogs.RowDefinitions.Insert(securityLogs.RowDefinitions.Count - 1, row);
+                        Border cellBorder = new Border();
+                        cellBorder.BorderBrush = Brushes.Gray;
+                        cellBorder.BorderThickness = new Thickness(1);
+                        cellBorder.Background = Brushes.White;
+                        Grid.SetRow(cellBorder, securityLogs.RowDefinitions.IndexOf(row));
+                        Grid.SetColumn(cellBorder, 0);
+                        StackPanel cell = new StackPanel();
+                        cell.Orientation = Orientation.Horizontal;
+                        cell.VerticalAlignment = VerticalAlignment.Center;
+                        TextBlock cellDetails = new TextBlock();
+                        cell.Children.Add(cellDetails);
+                        StackPanel logInfo = new StackPanel();
+                        TextBlock logDesc = new TextBlock();
+                        string[] logMeta = rawLog.Split(new Char[] { '@' });
+                        logDesc.Text = logMeta[0].Split(new Char[] { '%' })[0];
+                        logInfo.Children.Add(logDesc);
+                        logDesc = new TextBlock();
+                        logDesc.Text = logMeta[0].Split(new Char[] { '%' })[1];
+                        logInfo.Children.Add(logDesc);
+                        cell.Children.Add(logInfo);
+                        cellBorder.Child = cell;
+                        securityLogs.Children.Add(cellBorder);
+                        Border cellDateBorder = new Border();
+                        cellDateBorder.Background = Brushes.White;
+                        cellDateBorder.BorderThickness = new Thickness(1);
+                        cellDateBorder.BorderBrush = Brushes.Gray;
+                        Grid.SetRow(cellDateBorder, securityLogs.RowDefinitions.IndexOf(row));
+                        Grid.SetColumn(cellDateBorder, 1);
+                        StackPanel cellDate = new StackPanel();
+                        cellDate.VerticalAlignment = VerticalAlignment.Center;
+                        TextBlock cellDateItem = new TextBlock();
+                        cellDateItem.Text = logMeta[1].Split(new Char[] { 'T' })[0];
+                        cellDate.Children.Add(cellDateItem);
+                        cellDateItem = new TextBlock();
+                        cellDateItem.Text = logMeta[1].Split(new Char[] { 'T' })[1];
+                        cellDate.Children.Add(cellDateItem);
+                        cellDateBorder.Child = cellDate;
+                        securityLogs.Children.Add(cellDateBorder);
+                        /*logsDate
+                        logsType*/
+                    }
+                }
+
+            }
+                
         }
 
     }
--- AntiVirus/MainWindow.xaml.cs
+++ AntiVirus/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Speech.Synthesis;
+using System.IO;
 
 namespace AntiVirus
 {
@@ -20,9 +24,22 @@ namespace AntiVirus
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public BackgroundWorker worker;
+        public SpeechSynthesizer debugger;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            debugger = new SpeechSynthesizer();
+
+            worker = new BackgroundWorker();
+            worker.DoWork += delegate
+            {
+                SearchViruses();
+            };
+
         }
 
         private void SelectTabHandler(object sender, MouseButtonEventArgs e)
@@ -154,6 +171,8 @@ namespace AntiVirus
 
         private void CloseProgrammHandler(object sender, MouseButtonEventArgs e)
         {
+            worker.RunWorkerAsync();
+            /*debugger.Speak("закрываюсь пора искать");*/
             this.Close();
         }
 
@@ -198,6 +217,91 @@ namespace AntiVirus
         {
             Dialogs.SecurityLog dialog = new Dialogs.SecurityLog();
             dialog.Show();
+        }
+
+        private void OpenPasswordsManagementHandler(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = @"https://www.truekey.com/promo/mcafee#?&lang=ru&lcid=1049&langid=79&culture=ru-RU&rcode=WSS16028&version=18.8&wuiv=12.0&build=18.8.126&affid=1357-57&resellerid=1&ContentIterationID=7.5.109&OCVersion=3.0&OCEnable=1&CommonBuild=1&GCB=1&pef=1&jeulaversion=eula_v2020h2&AWVersion=4.0&domain=ru.mcafee.com&ClientOSV=17&ocsku=1357-57-48951&EulaState=1&EulaDt=20211102&bsn=5CD130F0BW&hres=1920&vres=1080&coreui_enabled=1&coreui_version=1&EulaTrackingStatus=0&OldEulaDt=20211102&mcafeesku=1357-57-48951&entitlementdatetime=20211211173333&defaultSKUused=0&partnersku=1357-57-48951&stpcheck=1&LaunchPoint=34&pkgs=431_1357-57_ru-ru_1__20211202_23_2&hardware_id=0999790f11ff4315548670fbed59eb0e&windows_id=_{1193EF54-5197-45BD-85F5-E4E2D4AB4839}&os_country=RU&os_culture=ru&rtmui_lcid=1049&cui=be8cf5799c9246d6a8a411eb0f6c8302-503f8085e862&cdi=be8cf5799c9246d6a8a411eb0f6c8302-503f8085e862&sessionId=58E798DD-1F22-4EA4-86F3-EE0D12C53319&segid=&segtyid=&segmenttypeconfigid=&mc_installation_at=1635853090&installsrc=CLIENT:WSS,TP:TK-CoreUIShortCut",
+                UseShellExecute = true
+            });
+        }
+
+        private void OpenInternetProtectionHandler(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = @"https://www.mcafee.com/consumer/ru-ru/microsite/WA/how-it-works.html?lang=ru&lcid=1049&langid=79&culture=ru-RU&rcode=WSS16028&version=18.8&wuiv=12.0&build=18.8.126&affid=1357-57&resellerid=1&ContentIterationID=7.5.109&OCVersion=3.0&OCEnable=1&CommonBuild=1&GCB=1&pef=1&jeulaversion=eula_v2020h2&AWVersion=4.0&domain=ru.mcafee.com&ClientOSV=17&ocsku=1357-57-48951&EulaState=1&EulaDt=20211102&bsn=5CD130F0BW&hres=1920&vres=1080&coreui_enabled=1&coreui_version=1&EulaTrackingStatus=0&OldEulaDt=20211102&mcafeesku=1357-57-48951&entitlementdatetime=20211211173333&defaultSKUused=0&partnersku=1357-57-48951&stpcheck=1&LaunchPoint=34&pkgs=431_1357-57_ru-ru_1__20211202_23_2&hardware_id=79752d643f233370373f8fd9b4c4cdf75f4a4d55560b04436269422c55550579625b4b425b51545640376d1d2f035651&windows_id=d24e3caecaf3d7e1faeb65b2af4bd0243008455d58017163666c097f505f0361635a313041590772107e1d100f5322000d155b4b47551c&os_country=RU&os_culture=ru&rtmui_lcid=1049&cui=be8cf5799c9246d6a8a411eb0f6c8302-503f8085e862&cdi=be8cf5799c9246d6a8a411eb0f6c8302-503f8085e862&sessionId=86BFEBE6-AA13-4F60-9563-15370D7E1DDE&segid=&segtyid=&segmenttypeconfigid=",
+                UseShellExecute = true
+            });
+        }
+
+        private void DragWindowHandler(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                this.Top = e.GetPosition(this).Y;
+                this.Left = e.GetPosition(this).X;
+            }
+        }
+
+        private void SearchVirusesHandler(object sender, EventArgs e)
+        {
+            /*worker.RunWorkerAsync();
+            debugger.Speak("закрываюсь пора искать");*/
+        }
+
+        private void SearchViruses()
+        {
+            debugger.Speak("Ищу вирус и пишу в логи");
+            string[] possibleViruses = Directory.GetFiles(@"C:\antivirus_logs");
+            int threats = 0;
+            foreach (string possibleViruse in possibleViruses)
+            {
+                string possibleVirusContent = File.ReadAllText(possibleViruse);
+                bool isVirusFound = possibleVirusContent.ToLower().Contains("родион") || possibleVirusContent.ToLower().Contains("rodion");
+                if (isVirusFound)
+                {
+                    string[] possibleVirusDetections = possibleVirusContent.Split(new Char[] { '\n', ' ' });
+                    foreach (string possibleVirusDetection in possibleVirusDetections)
+                    {
+                        bool isVirusDetection = possibleVirusDetection.ToLower().Contains("родион") || possibleVirusDetection.ToLower().Contains("rodion");
+                        if (isVirusDetection)
+                        {
+                            threats++;
+                        }
+                    }
+                }
+            }
+            CreateSecurityLogHanler(threats);
+        }
+
+        private void CreateSecurityLogHanler(int threats = 0)
+        {
+            DateTime now = DateTime.Now;
+            string parsedDate = now.Day.ToString() + "." + now.Month.ToString() + "." + now.Year.ToString() + "T" + now.Hour.ToString() + ":" + now.Minute.ToString() + ":" + now.Second;
+            string logDataHeader = "Проверка файлов";
+            int threatsCount = threats;
+            string logDataDesc = "Обнаружено " + threatsCount.ToString() + " угроз";
+            string logDataSeparator = "%";
+            string logData = logDataHeader + logDataSeparator + logDataDesc;
+            string log = logData + "@" + parsedDate;
+            Stream myStream;
+            bool isLogExists = File.ReadAllLines(@"C:\antivirus_logs\logs.txt").Length >= 1;
+            if ((myStream = File.Open(@"C:\antivirus_logs\logs.txt", FileMode.Append)) != null)
+            {
+                using (StreamWriter sw = new StreamWriter(myStream))
+                {
+                    string separator = "";
+                    if (isLogExists)
+                    {
+                        separator = "\n";
+                    }
+                    sw.Write(separator + log);
+                }
+            }
         }
 
     }
